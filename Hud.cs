84964583a12983cd41e76b6dcf6721b00b8f0daa
Jung <daejung@ea.com>
2022-06-20 14:25:04 +0900

    ing

--- Hud.cs
+++ Hud.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 using UnityEngine.UI;
 public class Hud : MonoBehaviour
 {
-    public GameObject TopLeft, TopRight, Left, Bottom;
-    public Vector2 Margin;
+    public GameObject TopLeft, TopRight, Left, Bottom, Ask;
+    public Vector2 Margin = new Vector2(5,10);
     public Vector2 TopLeftSize = new Vector2(160, 40);
     public Vector2 TopRightSize = new Vector2(400, 40); 
-    public Vector2 LeftSize = new Vector2(240, 240); 
-    public Vector2 BottomSize = new Vector2(700, 80);
+    public Vector2 LeftSize = new Vector2(200, 300); 
+    public Vector2 BottomSize = new Vector2(700, 60);
+    public Vector2 AskSize = new Vector2(400, 300);
+
     // Start is called before the first frame update
     void Start()
     {
@@ -23,14 +25,21 @@ public class Hud : MonoBehaviour
         750 : 80 = height : y
         = (80 / 750) * height
         = (700.0f / 1334.0f) * w
+        가로 길이를 구한다.
+        1366 : BottomSize.x = Screen.width : x
+        x = (BottomSize.x / 1334) * Screen.width 
+        세로 길이를 구한다.
+        BottomSize.x : BottomSize.y = x : y
+        y = (BottomSize.y / BottomSize.x) * x
         */
         RectTransform bottomRT = Bottom.GetComponent<RectTransform>();
-        bottomRT.sizeDelta = new Vector2((BottomSize.x / 1334.0f) * Screen.width, ((BottomSize.y / 750.0f) * Screen.height) + safe.y);
+        float x = (BottomSize.x / 1334.0f) * Screen.width;
+        bottomRT.sizeDelta = new Vector2(x, ((BottomSize.y / BottomSize.x) * x));
         bottomRT.anchoredPosition = new Vector2(0, safe.y);
 
         /*
         Left
-        Left-Center
+        Left-Middle
         750 : 240 = height : y
         = (240 / 750) * h
         1334 : 240 = width : x
@@ -62,7 +71,19 @@ public class Hud : MonoBehaviour
         topRightRT.sizeDelta = new Vector2((TopRightSize.x / 1334.0f) * Screen.width, (TopRightSize.y / 750.0f) * Screen.height);
         topRightRT.anchoredPosition = new Vector2(safe.x + safe.width - Margin.x - topRightRT.sizeDelta.x, safe.y + safe.height - Margin.y);
         
-        
+        /*
+        Ask
+        Center-Middle
+        가로 길이를 구한다.
+        1334 : AskSize.x = Screen.width : x
+        x = (AskSize.x / 1334) * Screen.width 
+        세로 길이를 구한다.
+        AskSize.x : AskSize.y = x : y
+        y = (AskSize.y / AskSize.x) * x
+        */
+        RectTransform askRT = Ask.GetComponent<RectTransform>();
+        x = (AskSize.x / 1334.0f) * Screen.width;
+        askRT.sizeDelta = new Vector2(x, ((AskSize.y / AskSize.x) * x));
     }
 
     // Update is called once per frame
